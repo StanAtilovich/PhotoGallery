c92b7e4831548375597d9cdb44bbcbc3f952c37c
StanAtilovich <alano92@yandex.ru>
2024-01-06 10:29:50 +0800

    finish character23 Challenge

--- app/src/main/java/ru/stan/photogallery/PhotoPageFragment.kt
+++ app/src/main/java/ru/stan/photogallery/PhotoPageFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
@@ -15,6 +16,7 @@ import ru.stan.photogallery.databinding.FragmentPhotoPageBinding
 
 class PhotoPageFragment : Fragment() {
     private val args: PhotoPageFragmentArgs by navArgs()
+    private lateinit var callback: OnBackPressedCallback
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(
@@ -27,36 +29,59 @@ class PhotoPageFragment : Fragment() {
             container,
             false
         )
-        binding.apply {
-            progressBar.max = 100
-            webView.apply {
-                settings.javaScriptEnabled = true
-                webViewClient = WebViewClient()
-                loadUrl(args.photoPageUri.toString())
+        val webView = binding.webView
+        val progressBar = binding.progressBar
 
-                webChromeClient = object : WebChromeClient() {
-                    override fun onProgressChanged(
-                        webView: WebView,
-                        newProgress: Int
-                    ) {
-                        if (newProgress == 100) {
-                            progressBar.visibility = View.GONE
-                        } else {
-                            progressBar.visibility = View.VISIBLE
-                            progressBar.progress = newProgress
-                        }
-                    }
-
-                    override fun onReceivedTitle(
-                        view: WebView?,
-                        title: String?
-                    ) {
-                        val parent = requireActivity() as AppCompatActivity
-                        parent.supportActionBar?.subtitle = title
-                    }
+        callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    isEnabled = false
+                    requireActivity().onBackPressedDispatcher.onBackPressed()
                 }
             }
         }
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner, callback
+        )
+
+        progressBar.max = 100
+        webView.apply {
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+            loadUrl(args.photoPageUri.toString())
+
+            webChromeClient = object : WebChromeClient() {
+                override fun onProgressChanged(
+                    webView: WebView,
+                    newProgress: Int
+                ) {
+                    if (newProgress == 100) {
+                        progressBar.visibility = View.GONE
+                    } else {
+                        progressBar.visibility = View.VISIBLE
+                        progressBar.progress = newProgress
+                    }
+                }
+
+                override fun onReceivedTitle(
+                    view: WebView?,
+                    title: String?
+                ) {
+                    val parent = requireActivity() as AppCompatActivity
+                    parent.supportActionBar?.subtitle = title
+                }
+            }
+
+        }
         return binding.root
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+            callback.remove()
+    }
 }
+
+
